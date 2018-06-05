71f122aaa397fc108ed63f3a116aee43e3fab43c
Kapil <kapilbaraskar91@gmail.com>
2018-06-06 00:02:08 +0530

    React Application

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import Auxx from '../../hoc/Auxx'
+
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Auxx>
+                <div>Burger</div>
+                <div>Burger Controls</div>
+            </Auxx>
+        );
+
+    }
+}
+
+export default BurgerBuilder
