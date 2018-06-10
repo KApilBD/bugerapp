17f18a68a69d4ef028db71009a2406d96041c382
Kapil <kapilbaraskar91@gmail.com>
2018-06-10 23:46:42 +0530

    React App

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React,{Fragment} from "react";
+import Backdrop from '../Backdrop/Backdrop';
+import classes from './Modal.css';
 
-import classes from './Modal.css'
+
 
 const modal = (props) => (
-    <div className={classes.Modal}>
-        {props.children}
-    </div>
+    <Fragment>
+        <Backdrop show ={props.show} clicked = {props.modalClosed}/>
+        <div 
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)':'translateY(-100vh)',
+                opacity: props.show ? '1': '0',
+            }}>
+            {props.children}
+        </div>
+    </Fragment>
 );
 
 export default modal;
