0b8581d6b5b02257bbdbec0ffb614a62864d17d1
Kapil <kapilbaraskar91@gmail.com>
2018-06-17 22:37:19 +0530

    React App

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import BurgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css'
 
 const logo = (props) => (
-    <div className={classes.Logo}> 
+    <div className={classes.Logo} style={{height:props.height}}> 
         {/* <img src="../../assets/images/butger-logo.png"/> */}
         <img src={BurgerLogo} alt ="my Burger"/>
 
