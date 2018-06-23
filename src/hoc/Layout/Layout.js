dca1141d007075f33a88377817dee904c29dbba0
Kapil <kapilbaraskar91@gmail.com>
2018-06-23 16:15:36 +0530

    Burger App

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,8 +2,8 @@ import React,{Fragment,Component} from 'react';
 //import Auxx from '../../hoc/Auxx';
 import classes from './Layout.css';
 
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 
 class Layout extends Component{
